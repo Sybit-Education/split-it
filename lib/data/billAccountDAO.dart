bb2372302c47d54d84c3b5b7d3305f162ac5f08f
Andreas Partsch <Andreas.Partsch@sybit.de>
2019-12-23 12:21:47 +0100

    Added billAccountDAo

---
+++ lib/data/billAccountDAO.dart
@@ -0,0 +1,48 @@
+import 'dart:async';
+
+import 'package:split_it/data/bill_account.dart';
+import 'package:sqflite/sqflite.dart';
+
+class BillAccountDAO {
+  Database database;
+
+  BillAccountDAO(Database db) {
+    this.database = db;
+  }
+
+  Future<void> insert(BillAccount billAccount) async {
+    await database.insert(
+      'billAccount',
+      billAccount.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  Future<List<BillAccount>> list() async {
+    final List<Map<String, dynamic>> maps = await database.query('billAccount');
+
+    return List.generate(maps.length, (i) {
+      return BillAccount(
+          id: maps[i]['id'],
+          eventId: maps[i]['eventId'],
+          personId: maps[i]['personId']);
+    });
+  }
+
+  Future<void> update(BillAccount billAccount) async {
+    await database.update(
+      'billAccount',
+      billAccount.toMap(),
+      where: "id = ?",
+      whereArgs: [billAccount.id],
+    );
+  }
+
+  Future<void> delete(int id) async {
+    await database.delete(
+      'billAccount',
+      where: "id = ?",
+      whereArgs: [id],
+    );
+  }
+}
