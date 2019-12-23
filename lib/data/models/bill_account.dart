4acc1e519abff4c3c8bed2fedc2f516f2d2ce533
Andreas Partsch <Andreas.Partsch@sybit.de>
2019-12-23 13:34:35 +0100

    Removed comments

--- lib/data/models/bill_account.dart
+++ lib/data/models/bill_account.dart
@@ -7,8 +7,6 @@ class BillAccount {
 
   BillAccount({this.id, this.eventId, this.personId, this.description, this.amount});
 
-  // Convert a Dog into a Map. The keys must correspond to the names of the
-  // columns in the database.
   Map<String, dynamic> toMap() {
     return {
       'id': id,
