091afe6c91f6946f9f83bf72857cec359cc36fc2
stritti <Stephan@st-strittmatter.name>
2019-12-23 11:25:07 +0100

    add to Map

--- lib/data/bill_account.dart
+++ lib/data/bill_account.dart
@@ -6,4 +6,14 @@ class BillAccount {
   double amount;
 
   BillAccount({this.id, this.eventId, this.personId});
+
+  // Convert a Dog into a Map. The keys must correspond to the names of the
+  // columns in the database.
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'description': description,
+      'amount': amount,
+    };
+  }
 }
--- lib/data/event.dart
+++ lib/data/event.dart
@@ -3,4 +3,13 @@ class Event {
   final String name;
 
   Event({this.id, this.name});
+
+  // Convert into a Map. The keys must correspond to the names of the
+  // columns in the database.
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+    };
+  }
 }
